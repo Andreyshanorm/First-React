ad7dc8540c539d521c729cae775eb12b5f62e3cb
Andreyshanorm <okeenlen578@gmail.com>
2023-09-14 08:44:28 +0400

    reload

--- src/App/Achievments/Main/SectionCenter/SectionCenter.js
+++ src/App/Achievments/Main/SectionCenter/SectionCenter.js
@@ -1,14 +1,41 @@
 import './SectionCenter.css';
+import { Cards } from "./data/acheivments"
+
 
 
 function SectionCenter() {
     return (
-        <div className="main-img__block">
-          <div className="main-img__text">
-            <p>
-              Achievment
-            </p>
-            <p className="signature">Artwork generated with midjourney</p>
+        <div className="main-acheiv__block">
+          <div className="main-acheiv__content">
+            <h2 className="achiev-content__title">Achievments</h2>
+            <div className="filter__block">
+              <p className="filter__title">Filter</p>
+              <div className="filter__menu">
+                <label className="in-progress filter active">
+                  <input type="checkbox" className="toggle__real"/>
+                  <span className="toggle__fake"></span>
+                  <p>Achieved</p>
+                </label>
+                <label className="achieved filter">
+                  <input type="checkbox" className="toggle__real"/>
+                  <span className="toggle__fake"></span>
+                  <p>To DO</p>
+                </label>
+              </div>
+            </div>
+            <div className="cards-list__container">
+              <div className="cards-list">
+                {Cards.map((item) => 
+                  <div className={item.classLink}>
+                    <div className="card-list-item__img-container">
+                      <img src={item.img} alt="" />
+                      <div className="card__tag"><span>{item.type}</span></div>
+                    </div>
+                    <h3 className="card-list-item__title">{item.text}</h3>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
     );
---
+++ src/App/Achievments/Main/SectionCenter/data/acheivments.js
@@ -0,0 +1,57 @@
+export const Cards = [
+    {
+        "type": "legendary",
+        "img": require("./leg_img.png"),
+        "text": "1000 stars on my project",
+        "opacity": "",
+        "classLink": "cards-list__item legendary"
+    },
+    {
+        "type": "epic",
+        "img": require("./epic_img.png"),
+        "text": "Release personal website",
+        "opacity": "",
+        "classLink": "cards-list__item epic"
+    }
+    ,
+    {
+        "type": "epic",
+        "img": require("./epic_img.png"),
+        "text": "developed my Open source plugin",
+        "opacity": "",
+        "classLink": "cards-list__item epic"
+    }
+    ,
+    {
+        "type": "rare",
+        "img": require("./rare_img.png"),
+        "text": "Master of Markup",
+        "opacity": "",
+        "classLink": "cards-list__item rare"
+    }
+    ,
+    {
+        "type": "rare",
+        "img": require("./rare_img.png"),
+        "text": "Pixel-Perfect Perfectionist",
+        "opacity": "",
+        "classLink": "cards-list__item rare"
+    }
+    ,
+    {
+        "type": "rare",
+        "img": require("./rare_img.png"),
+        "text": "Speed Demon",
+        "opacity": "",
+        "classLink": "cards-list__item rare"
+    }
+    ,
+    {
+        "type": "epic",
+        "img": require("./rare_img.png"),
+        "text": "another awesome milestine",
+        "opacity": "semi-hidden",
+        "classLink": "cards-list__item rare"
+    }
+    
+]
